7072eed48852fca43b0ebe588864bdcb9e6e8585
paddykadnor <kadnorpradeep@live.com>
2021-01-14 01:29:04 +0530

    web scoket

--- src/group.js
+++ src/group.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import propTypes from "prop-types"
 import './style.css';
-import Header from "./components/Header/Header"
-import Category from "./components/Category/Category"
 import { Grid, Button, TextField, Container, Box, Divider } from '@material-ui/core'
 import { connect } from "react-redux";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons'
 import { getBucket, saveBucket } from "./actions/bucket.action"
 import { getTask, saveTask, removeTask, completeTask, editTask, updateTaskName } from "./actions/task.action"
+import { findIndex, filter, cloneDeep } from 'lodash'
 
 
 class App extends Component {
@@ -23,16 +22,44 @@ class App extends Component {
     }
 
     componentDidMount() {
-         fetch("http://localhost:4200/group", {
-            method: "post",
-            data: { exportFormat: "XML" }
-        }).then((response) => {
-            console.log(response)
-        })
+
+        // var W3CWebSocket = require('websocket').w3cwebsocket;
+
+        // var client = new W3CWebSocket('ws://localhost:8080/', 'echo-protocol');
+
+        // client.onerror = function () {
+        //     console.log('Connection Error');
+        // };
+
+        // client.onopen = function () {
+        //     console.log('WebSocket Client Connected');
+
+        //     function sendNumber() {
+        //         if (client.readyState === client.OPEN) {
+        //             var number = Math.round(Math.random() * 0xFFFFFF);
+        //             client.send(number.toString());
+        //             setTimeout(sendNumber, 1000);
+        //         }
+        //     }
+        //     sendNumber();
+        // };
+
+        // client.onclose = function () {
+        //     console.log('echo-protocol Client Closed');
+        // };
+
+        // client.onmessage = function (e) {
+        //     if (typeof e.data === 'string') {
+        //         console.log("Received: '" + e.data + "'");
+        //     }
+        // };
+        this.fetchGroup()
     }
 
-    createGroup = async () => {
+    createGroup = async (action) => {
         const { groupName } = this.state
+        // const action = e.currentTarget.name
+        //  e.stopPropagation()
         await fetch("http://localhost:4200/group/create", {
             method: "post",
             headers: {
@@ -41,7 +68,7 @@ class App extends Component {
             mode: 'cors', // no-cors, *cors, same-origin
             cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
             credentials: 'same-origin', // include, *same-origin, omit
-            body: JSON.stringify({ "groupName": groupName })
+            body: JSON.stringify({ "groupName": groupName, action })
 
         }).then(response => {
             if (response.status === 200) {
@@ -49,14 +76,27 @@ class App extends Component {
                     groupName: ""
                 })
             }
-            console.log(response)
+            // console.log(response)
         })
 
-        await fetch("http://localhost:4200/group", {
+        this.fetchGroup()
+    }
+
+    fetchGroup = () => {
+        fetch("http://localhost:4200/group", {
             method: "post",
             data: { exportFormat: "XML" }
         }).then((response) => {
-            console.log(response)
+            return response.json()
+        }).then(data => {
+            const groups = []
+            data.elements[0].elements[1].elements[1].elements.map((ele, index) => {
+                // console.log(ele.elements[0].attributes.NAME)
+                groups.push({ Id: index, group: ele.elements[0].attributes.NAME })
+            })
+            this.setState({
+                groups
+            })
         })
     }
 
@@ -65,8 +105,28 @@ class App extends Component {
             groupName: e.target.value
         })
     }
+
+    delete = async (e) => {
+        const { groups } = this.state
+        e.persist();
+        const arr = [...groups]
+        const Id = e.currentTarget.id
+        // e.stopPropagation()
+        const index = findIndex(groups, { 'Id': parseInt(Id) })
+        // console.log(groups.splice(index, 1))
+
+        this.setState({
+            groupName: groups[index].group,
+            //  groups: index >= 0 ? groups.splice(index, 1) : groups
+        }, () => {
+            this.createGroup("Delete")
+        })
+
+
+
+    }
     render() {
-        const { groupName } = this.state
+        const { groupName, groups } = this.state
         return (
             <Container maxWidth="md" >
                 <Box spacing={2} paddingTop={2}>
@@ -75,7 +135,7 @@ class App extends Component {
                             <TextField id="standard-basic" label="Group Name" variant="outlined" onChange={this.setgroupName} value={groupName} size="small" />
                         </Grid>
                         <Grid item xs={6} style={{ textAlign: "right" }}>
-                            <Button color="primary" variant="contained" size="small" disabled={groupName.length > 0 ? false : true} onClick={this.createGroup}>
+                            <Button color="primary" variant="contained" size="small" disabled={groupName.length > 0 ? false : true} name="Create" onClick={() => this.createGroup("Create")}>
                                 Create Group
                         </Button>
                         </Grid>
@@ -85,15 +145,28 @@ class App extends Component {
                     <Divider />
                     <br />
                     <Grid container spacing={2}>
-                        <Grid item xs={6}>
-                            Hello
+                        {
+                            groups.map(name => {
+                                return (
+                                    <React.Fragment>
+                                        <Grid item xs={8}>
+                                            {name.group}
+                                        </Grid>
+                                        <Grid item xs={4} style={{ textAlign: "right" }}>
+                                            <Button variant="contained" size="small" name="Delete" id={name.Id} onClick={this.delete} color="secondary">Delete</Button>
+                                        </Grid>
+                                    </React.Fragment>
+                                )
+                            })
+                        }
+
+
                     </Grid>
-                        <Grid item xs={6} style={{ textAlign: "right" }}>
-                            Hello
-                    </Grid>
-                    </Grid>
+                    <br />
+                    <Divider />
+                    <br />
                 </Box>
-            </Container>
+            </Container >
         );
     }
 }
